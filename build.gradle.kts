4323108c2f7578fa0e580657c0b1de79f9c2f597
Eric <192110+scribe@users.noreply.github.com>
2019-04-09 16:14:22 -0600

    Working command line

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@
  *   Copyright 2014-2019 Spectra Logic Corporation. All Rights Reserved.
  * ***************************************************************************
  */
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
-
     application
 }
 
@@ -14,8 +14,14 @@ repositories {
     jcenter()
 }
 
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${project.property("kotlinxCoroutines")}")
     implementation("com.github.ajalt:clikt:${project.property("clikt")}")
     implementation("io.github.microutils:kotlin-logging:${project.property("kotlinLogging")}")
     implementation("org.jlleitschuh.guice:kotlin-guiced-core:${project.property("kotlinGuiced")}")
@@ -26,5 +32,5 @@ dependencies {
 }
 
 application {
-    mainClassName = "bp.bench.AppKt"
+    mainClassName = "com.spectralogic.bp.bench.AppKt"
 }
--- src/main/kotlin/com/spectralogic/bp/bench/cli/MainCommand.kt
+++ src/main/kotlin/com/spectralogic/bp/bench/cli/MainCommand.kt
@@ -1,11 +1,20 @@
+/*
+ * ****************************************************************************
+ *   Copyright 2014-2019 Spectra Logic Corporation. All Rights Reserved.
+ * ***************************************************************************
+ */
 package com.spectralogic.bp.bench.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.google.inject.Guice
-import org.jlleitschuh.guice.module
+import com.github.ajalt.clikt.core.findObject
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 
-class MainCommand: CliktCommand() {
+class MainCommand : CliktCommand(name = "bpBench") {
+    val verbose by option("-v", "--verbose", help = "enable verbose output").flag()
+
     override fun run() {
-
+        findObject { verbose }
+        echo("Test")
     }
 }
