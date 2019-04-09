136a0fcf0206c3e1de08cc11ff3cf8cff5ca1af3
Eric <192110+scribe@users.noreply.github.com>
2019-04-09 12:38:02 -0600

    I think I did this right

---
+++ src/main/kotlin/com/spectralogic/bp/bench/App.kt
@@ -0,0 +1,10 @@
+/*
+ * ****************************************************************************
+ *   Copyright 2014-2019 Spectra Logic Corporation. All Rights Reserved.
+ * ***************************************************************************
+ */
+package com.spectralogic.bp.bench
+
+fun main(args: Array<String>) {
+    println("Hello World")
+}
