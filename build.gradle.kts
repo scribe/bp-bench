26cf73c5476b106de8e97251e1a825579f6bef65
Eric <192110+scribe@users.noreply.github.com>
2019-04-09 15:38:40 -0600

    Adding scaffolding

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,13 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.ajalt:clikt:${project.property("clikt")}")
+    implementation("io.github.microutils:kotlin-logging:${project.property("kotlinLogging")}")
+    implementation("org.jlleitschuh.guice:kotlin-guiced-core:${project.property("kotlinGuiced")}")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.assertj:assertj-core:${project.property("assertj")}")
 }
 
 application {
---
+++ src/main/kotlin/com/spectralogic/bp/bench/cli/MainCommand.kt
@@ -0,0 +1,11 @@
+package com.spectralogic.bp.bench.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.google.inject.Guice
+import org.jlleitschuh.guice.module
+
+class MainCommand: CliktCommand() {
+    override fun run() {
+
+    }
+}
